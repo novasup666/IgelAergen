5bba798105309f1442b2fd70feef6a3a0db10ed4
AdilOub <adil.oubninte.pro@gmail.com>
2024-10-15 15:35:41 +0200

    gitignore & cmake

--- src/util.c
+++ src/util.c
@@ -6,21 +6,26 @@ int readInt(int size){
     char buffer[size];
     int n = 0;
     while(n<size){
-        int c = getc();
+        int c = getc(stdin);
+        printf("char: %c\n", c);
         if(c == EOF){
-            return -1
+            return -1; //not suppose to happen
         }
-        buffer[n] = c;
+        if(c=='\n'){
+            break;
+        }
+        buffer[n] = 0;
         n++;
     }
     if(n==size){
-        return -2;
+        return -2; //depassement de capacité
     }
-    return atoi(buffer)
+    return atoi(buffer);
 }
 
 int main(){
+    printf("Une valeur stp:\n>");
     int n = readInt(5);
-    printf("valeur de n: %d"\n, n);
+    printf("valeur de n: %d\n", n);
     return 0;
 }
